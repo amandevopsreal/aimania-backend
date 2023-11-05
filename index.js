e491891fd8b61adbaaa1230f6ec5440941f94dce
amandevopsreal <aman345kumarmishra@gmail.com>
2023-11-05 04:53:23 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const connectToMngo = require("./db");
+const express = require("express")
+const cors = require("cors");
+
+connectToMngo();
+const app = express()
+const port = 5000;
+
+app.use(express.json())
+app.use(cors());
+
+//Available routes
+app.use("/api/auth", require('./routes/auth'))
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`)
+})
+
